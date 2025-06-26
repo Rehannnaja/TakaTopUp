cd549d501051709048393817f3f20873a5dd5931
!Taka dev <kontol543215@gmail.com>
2025-06-26 18:05:30 +0700

    Update games.ts

--- src/data/games.ts
+++ src/data/games.ts
@@ -1,1 +1,41 @@
+const games = [
+  {
+    id: 'mlbb',
+    name: 'Mobile Legends',
+    description: 'Top up Diamond MLBB dengan cepat dan mudah. Cocok untuk para pejuang Land of Dawn!',
+    image: 'mlbb.png',
+  },
+  {
+    id: 'wuthering',
+    name: 'Wuthering Waves',
+    description: 'Top up premium currency untuk petualanganmu di dunia Solaris-3.',
+    image: 'wutheringwaves.png',
+  },
+  {
+    id: 'genshin',
+    name: 'Genshin Impact',
+    description: 'Top up Genesis Crystals dan nikmati petualangan epik di Teyvat.',
+    image: 'genshin.png',
+  },
+  {
+    id: 'pubg',
+    name: 'PUBG Mobile',
+    description: 'Top up UC untuk skin senjata, kendaraan, dan elite pass favoritmu.',
+    image: 'pubg.png',
+  },
+  {
+    id: 'pgr',
+    name: 'Punishing: Gray Raven',
+    description: 'Isi KuroCards untuk memperkuat Construct favoritmu di medan tempur.',
+    image: 'pgr.png',
+  },
+  {
+    id: 'deltaforce',
+    name: 'Delta Force: Hawk Ops',
+    description: 'Top up CP dan dominasi medan perang dengan perlengkapan elite.',
+    image: 'deltaforce.png',
+  },
+]
+
+export default games
 
