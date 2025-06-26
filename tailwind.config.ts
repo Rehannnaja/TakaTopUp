b32715e51936d8a38478194631dff1ab738d7697
!Taka dev <kontol543215@gmail.com>
2025-06-26 20:46:55 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,6 @@ const config: Config = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
   ],
 }
