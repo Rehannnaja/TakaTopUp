6261ba590f110c1c5e6a462bf8a03236aa80d711
!Taka dev <kontol543215@gmail.com>
2025-06-26 17:58:35 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import GameDetail from './pages/GameDetail'
+import NotFound from './pages/NotFound'
+import MainLayout from './layout/MainLayout'
+
+export default function App() {
+  return (
+    <Router>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game/:id" element={<GameDetail />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </MainLayout>
+    </Router>
+  )
+}
 
