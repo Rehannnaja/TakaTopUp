9ea686b203929248974196909126ed26af88b9f6
!Taka dev <kontol543215@gmail.com>
2025-06-26 18:07:12 +0700

    Update packages.ts

--- src/data/packages.ts
+++ src/data/packages.ts
@@ -1,1 +1,35 @@
+const packages: Record<string, { name: string; price: number }[]> = {
+  mlbb: [
+    { name: '86 Diamond', price: 20000 },
+    { name: '172 Diamond', price: 39000 },
+    { name: '257 Diamond', price: 59000 },
+  ],
+  wuthering: [
+    { name: '60 Astrite', price: 16000 },
+    { name: '300 Astrite', price: 79000 },
+    { name: '980 Astrite', price: 249000 },
+  ],
+  genshin: [
+    { name: '60 Genesis Crystals', price: 15000 },
+    { name: '330 Genesis Crystals', price: 75000 },
+    { name: '6480 Genesis Crystals', price: 749000 },
+  ],
+  pubg: [
+    { name: '63 UC', price: 15000 },
+    { name: '340 UC', price: 79000 },
+    { name: '720 UC', price: 149000 },
+  ],
+  pgr: [
+    { name: '300 KuroCards', price: 50000 },
+    { name: '980 KuroCards', price: 159000 },
+    { name: '1980 KuroCards', price: 289000 },
+  ],
+  deltaforce: [
+    { name: '80 CP', price: 15000 },
+    { name: '240 CP', price: 45000 },
+    { name: '500 CP', price: 88000 },
+  ],
+}
+
+export default packages
 
